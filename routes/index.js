d77f46b5b812d3b939d10d48a330874a5bfb7994
marsdenmadoka <marsmadoka98@gmail.com>
2020-05-26 19:31:06 +0300

    fetched data from mongodb and displayed in in doctors..ejs

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+
+
 require('../models/User');
 var InsertRecord=mongoose.model('InsertRecord')
+
+mongoose.connect('mongodb://localhost:27017/myqueue'); 
+var db=mongoose.connection; 
+db.on('error', console.log.bind(console, "connection error")); 
+db.once('open', function(callback){ 
+    console.log("connection succeeded"); 
+}) 
+  
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('Login', { title: 'Express' });
 });
-
 
 router.get('/register', function(req, res, next) {
   res.render('Register', { title: 'Express' });
@@ -18,14 +32,30 @@ router.get('/index', function(req, res, next) {
 });
 
 
-router.get('/doctors',function(req,res,next){
+router.get('/patientdetails',function(req,res,next){
+data='';
 
-  InsertRecord.find({}, function (err, allDetails) {
-    if (err) {
-        console.log(err);
-    } else {
-        res.render("doctors", { details: allDetails })
+  db.collection("patientdetails").find({}).toArray(function(err,docs){
+    if(err){
+      console.log(err)
+    }else{
+      res.render('doctors',{data:docs})
     }
-})
+  })
+
+  // user.find({}, function(err, docs){
+	// 	if(err) res.json(err);
+	// 	else    res.render('index', {users: docs});
+	// });
+
+
+
+//   InsertRecord.find({}, function (err, records) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         res.render("doctors",{users:records });
+//     }
+// })
 });
 module.exports = router;
