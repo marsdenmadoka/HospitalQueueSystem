db9157693909e7289a04cc0a08aa3409462d21a8
marsdenmadoka <marsmadoka98@gmail.com>
2020-07-08 21:53:19 +0300

    added chnages

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -3,3 +3,24 @@
 // var code = access.innerHTML;
 // code = code + " midnight";
 // alert(code);
+
+var getphoneno=document.getElementById("PhoneNumber");
+ var phoneNo=getphoneno.innerHTML;
+
+
+ function sanitizer(){
+     if(phoneNo.length !==8){
+         return false;
+     }
+     for(var i=0;i < phoneNo.length; i++){
+         if(i===3){
+             if(phoneNo.charAt(i) !== '-'){
+                 return false;
+             }else if(isNaN(phoneNo.charAt(i))){
+                 return false;
+
+             }
+         }
+     }
+
+ }
